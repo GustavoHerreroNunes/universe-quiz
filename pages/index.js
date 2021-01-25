43c44c94536a1a39ed95edc55b149af1f0eea4ee
GustavoHerreroNunes <ghnunes2010@gmail.com>
2021-01-25 19:31:47 -0300

    Aula nº 1 - Criando componentes e configs. base para a Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,54 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Background from '../src/components/Background'
+import Logo from '../src/components/Logo'
+import Widget from '../src/components/Widget'
+import GithubCorner from '../src/components/GithubCorner'
+import Footer from '../src/components/Footer'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const Container = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
 
+  @media screen and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
+
+`;
+
+/*Componente que representa a página*/
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <Background backgroundImage={db.bg}>
+      <Container>
+        <GithubCorner />
+
+        <Logo />
+
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>  
+        </Widget>
+        
+        <Widget>
+          <Widget.Content>
+            <h1>Outras Aventuras</h1>
+
+            <p>www.aindanenhuma.com.:)</p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+          
+      </Container>
+    </Background>
+  );
 }
