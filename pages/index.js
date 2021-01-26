12646084d58235542cb7bbc4dd5fcd623611856a
GustavoHerreroNunes <ghnunes2010@gmail.com>
2021-01-26 16:41:04 -0300

    Extra: Deletando componente 'Container' da Home Page

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,6 @@ import Widget from '../src/components/Widget';
 
 const React = require('react');
 
-export const Container = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-
-  @media screen and (max-width: 500px){
-    margin: auto;
-    padding: 15px;
-  }
-
-`;
-
 /* Componente que representa a página */
 export default function Home() {
   const router = useRouter();
