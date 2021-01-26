7c5ec01e1c6e114cbdd3dce3d68fa7eb15bd88a1
GustavoHerreroNunes <ghnunes2010@gmail.com>
2021-01-26 20:11:17 -0300

    Aula nº 2: Desafio - Pegando 'name' passado via query param da URL

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import Widget from '../src/components/Widget';
 import PageDefault from '../src/components/PageDefault';
 
 export default function QuizPage() {
+  const router = useRouter();
+
   return (
     <PageDefault>
-      Página de Quiz
+      <Widget>
+        <Widget.Header>
+          <h1>Quiz em Construção</h1>
+        </Widget.Header>
+        <Widget.Content>
+          <h3>Olá {router.query.name}!!!</h3>
+          <p>O quiz está em contrução, em breve teremos as perguntas<br />Obrigado por acessar :)</p>
+        </Widget.Content>
+
+      </Widget>
     </PageDefault>
   );
 }
